30040ecfbab2a85f2f5af48f6fa3297249b7aed6
badcoffee <mbl@test.com>
2018-02-20 15:23:33 +0100

    ported to sdk 14.2.0

--- nrf5-mpu-data-ready-interrupts/main.c
+++ nrf5-mpu-data-ready-interrupts/main.c
@@ -1,101 +1,103 @@
- /* 
-  * This example is not extensively tested and only 
-  * meant as a simple explanation and for inspiration. 
-  * NO WARRANTY of ANY KIND is provided. 
-  */
+/**
+ * Copyright (c) 2009 - 2017, Nordic Semiconductor ASA
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form, except as embedded into a Nordic
+ *    Semiconductor ASA integrated circuit in a product or a software update for
+ *    such product, must reproduce the above copyright notice, this list of
+ *    conditions and the following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
+ * 
+ * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ * 
+ * 4. This software, with or without modification, must only be used with a
+ *    Nordic Semiconductor ASA integrated circuit.
+ * 
+ * 5. Any software provided in binary form under this license must not be reverse
+ *    engineered, decompiled, modified and/or disassembled.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+/** @file
+* @brief Example template project.
+* @defgroup nrf_templates_example Example Template
+*
+*/
 
-#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "nrf.h"
+#include "nordic_common.h"
 #include "boards.h"
-#include "app_util_platform.h"
-#include "app_uart.h"
-#include "app_error.h"
 #include "nrf_delay.h"
 #include "app_mpu.h"
 #include "nrf_drv_gpiote.h"
 
-#define MPU_MPU_INT_PIN     5
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
 
-/*UART buffer size. */
-#define UART_TX_BUF_SIZE 256
-#define UART_RX_BUF_SIZE 1
+#define MPU_MPU_INT_PIN     30
 
 volatile bool mpu_data_ready = false;
 
-/**
- * @brief UART events handler.
+
+/**@brief Function for initializing the nrf log module.
  */
-static void uart_events_handler(app_uart_evt_t * p_event)
+static void log_init(void)
 {
-    switch (p_event->evt_type)
-    {
-        case APP_UART_COMMUNICATION_ERROR:
-            APP_ERROR_HANDLER(p_event->data.error_communication);
-            break;
-
-        case APP_UART_FIFO_ERROR:
-            APP_ERROR_HANDLER(p_event->data.error_code);
-            break;
-
-        default:
-            break;
-    }
-}
-
-
-/**
- * @brief UART initialization.
- * Just the usual way. Nothing special here
- */
-static void uart_config(void)
-{
-    uint32_t                     err_code;
-    const app_uart_comm_params_t comm_params =
-    {
-        RX_PIN_NUMBER,
-        TX_PIN_NUMBER,
-        RTS_PIN_NUMBER,
-        CTS_PIN_NUMBER,
-        APP_UART_FLOW_CONTROL_DISABLED,
-        false,
-        UART_BAUDRATE_BAUDRATE_Baud115200
-    };
-
-    APP_UART_FIFO_INIT(&comm_params,
-                       UART_RX_BUF_SIZE,
-                       UART_TX_BUF_SIZE,
-                       uart_events_handler,
-                       APP_IRQ_PRIORITY_LOW,
-                       err_code);
-
+    ret_code_t err_code = NRF_LOG_INIT(NULL);
     APP_ERROR_CHECK(err_code);
+
+    NRF_LOG_DEFAULT_BACKENDS_INIT();
 }
 
 
-void mpu_setup(void)
+void mpu_init(void)
 {
     uint32_t err_code;
     // Initiate MPU driver
-    err_code = mpu_init();
+    err_code = app_mpu_init();
     APP_ERROR_CHECK(err_code); // Check for errors in return value
     
     // Setup and configure the MPU with intial values
-    mpu_config_t p_mpu_config = MPU_DEFAULT_CONFIG(); // Load default values
+    app_mpu_config_t p_mpu_config = MPU_DEFAULT_CONFIG(); // Load default values
     p_mpu_config.smplrt_div = 199;   // Change sampelrate. Sample Rate = Gyroscope Output Rate / (1 + SMPLRT_DIV). 199 gives a sample rate of 5Hz
     p_mpu_config.accel_config.afs_sel = AFS_2G; // Set accelerometer full scale range to 2G
-    err_code = mpu_config(&p_mpu_config); // Configure the MPU with above values
+    err_code = app_mpu_config(&p_mpu_config); // Configure the MPU with above values
     APP_ERROR_CHECK(err_code); // Check for errors in return value
     
     
     // This is a way to configure the interrupt pin behaviour
-    mpu_int_pin_cfg_t p_int_pin_cfg = MPU_DEFAULT_INT_PIN_CONFIG(); // Default configurations
+    app_mpu_int_pin_cfg_t p_int_pin_cfg = MPU_DEFAULT_INT_PIN_CONFIG(); // Default configurations
     p_int_pin_cfg.int_rd_clear = 1; // When this bit is equal to 1, interrupt status bits are cleared on any read operation
-    err_code = mpu_int_cfg_pin(&p_int_pin_cfg); // Configure pin behaviour
+    err_code = app_mpu_int_cfg_pin(&p_int_pin_cfg); // Configure pin behaviour
     APP_ERROR_CHECK(err_code); // Check for errors in return value
     
     // Enable the MPU interrupts
-    mpu_int_enable_t p_int_enable = MPU_DEFAULT_INT_ENABLE_CONFIG();
+    app_mpu_int_enable_t p_int_enable = MPU_DEFAULT_INT_ENABLE_CONFIG();
     p_int_enable.data_rdy_en = 1; // Trigger interrupt everytime new sensor values are available
-    err_code = mpu_int_enable(&p_int_enable); // Configure interrupts
+    err_code = app_mpu_int_enable(&p_int_enable); // Configure interrupts
     APP_ERROR_CHECK(err_code); // Check for errors in return value    
 }
 
@@ -116,9 +118,11 @@ void int_pin_handler(nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t action)
 static void gpiote_init(void)
 {
     uint32_t err_code;
-
-    err_code = nrf_drv_gpiote_init();
-    APP_ERROR_CHECK(err_code);
+    if (!nrf_drv_gpiote_is_init())
+    {
+        err_code = nrf_drv_gpiote_init();
+        APP_ERROR_CHECK(err_code);
+    }
     
     nrf_drv_gpiote_in_config_t in_config = GPIOTE_CONFIG_IN_SENSE_LOTOHI(true);
 
@@ -129,40 +133,44 @@ static void gpiote_init(void)
 }
 
 /**
- * @brief Function for main application entry.
+ * @brief Function for application main entry.
  */
 int main(void)
 {
     uint32_t err_code;
-    LEDS_CONFIGURE(LEDS_MASK);
-	LEDS_OFF(LEDS_MASK);
-    uart_config();
-    printf("\033[2J\033[;HMPU example with MPU generated data ready interrupts. Compiled @ %s.\r\n", __TIME__);
+    
+    // Initialize.
+    log_init();
+	NRF_LOG_INFO("\033[2J\033[;H"); // Clear screen
+    
     gpiote_init();
-    mpu_setup();
+    mpu_init();
+    
+    // Start execution.
+    NRF_LOG_INFO("MPU Data Ready example.");
     
     accel_values_t acc_values;
     uint32_t sample_number = 0;
         
     while(1)
     {
-        nrf_gpio_pin_set(LED_4); // Turn off LED 4 when CPU is sleeping. 
-        while(mpu_data_ready != true)
+        if (NRF_LOG_PROCESS() == false)
         {
-            // Make sure any pending events are cleared
-            __SEV();
-            __WFE();
-            // Enter System ON sleep mode
-            __WFE();          
+            while(mpu_data_ready != true)
+            {
+                // Make sure any pending events are cleared
+                __SEV();
+                __WFE();
+                // Enter System ON sleep mode
+                __WFE();          
+            }
+         
+            err_code = app_mpu_read_accel(&acc_values);
+            APP_ERROR_CHECK(err_code);
+            // Clear terminal and print values
+            NRF_LOG_INFO("\033[2J\033[;HSample # %d\r\nX: %06d\r\nY: %06d\r\nZ: %06d", ++sample_number, acc_values.x, acc_values.y, acc_values.z);
+            mpu_data_ready = false;
         }
-        nrf_gpio_pin_clear(LED_4); // Light LED 4 when CPU is working
-
-        err_code = mpu_read_accel(&acc_values);
-        APP_ERROR_CHECK(err_code);
-        // Clear terminal and print values
-        printf("\033[2J\033[;HSample # %d\r\nX: %06d\r\nY: %06d\r\nZ: %06d", ++sample_number, acc_values.x, acc_values.y, acc_values.z);
-        mpu_data_ready = false;
     }
 }
-
 /** @} */
